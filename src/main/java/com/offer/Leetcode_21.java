965062a39cb546fd3dc0c97a88e5d0bd316e742a
RanMaosong <maosongran@gmail.com>
2019-03-22 12:26:08 +0800

    反转链表

---
+++ src/main/java/com/offer/Leetcode_21.java
@@ -0,0 +1,30 @@
+package com.offer;
+
+
+/**
+ * 反转链表
+ * 题目描述
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ */
+public class Leetcode_21 {
+    public ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode res = head;
+        head = head.next;
+        res.next = null;
+
+        while (head != null) {
+            ListNode tmp = head;
+            head = head.next;
+            tmp.next = res;
+            res = tmp;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
